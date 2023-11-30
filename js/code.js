89f622241f65348e42c4b34e082828dfd8b2f3d2
Gijs de Lange <g.de.lange@rocmondriaan.nl>
2023-11-30 23:16:15 +0100

    code for the door working

--- js/code.js
+++ js/code.js
@@ -1,8 +1,44 @@
-console.log('test');
+const door = document.querySelector(".door");
+const numbersDivs = document.querySelectorAll(".number");
+const displayDiv = document.querySelector(".display");
+let code = "";
+const passcode = "5498";
+// element.addEventListener("click", toggleDoor);
 
-const element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
+numbersDivs.forEach((numberDiv) => {
+    numberDiv.addEventListener('click', addNumber);
+})
+
+function addNumber(event) {
+    // console.log(event.target.innerText);
+    code += event.target.innerText;
+    displayDiv.innerText = code;
+
+    if (code.length >= 4) {
+        checkCode()
+    }
+}
+
+function checkCode() {
+    if (code === passcode) {
+        flashDisplay('green');
+        toggleDoor();
+    } else {
+        flashDisplay('red');
+    }
+    code = ""
+    displayDiv.innerText = code;
+
+}
+
+function flashDisplay(color) {
+    displayDiv.classList.add(color);
+    setTimeout(() => {
+        displayDiv.classList.remove(color);
+    }, 1000);
+}
 
 function toggleDoor() {
-    element.classList.toggle("doorOpen");
+    door.classList.toggle("doorOpen");
 }
+
